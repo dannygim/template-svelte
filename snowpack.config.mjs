084444863846e93f96a9535bdc55a0bc4590f666
Danny Gim <danny.gim@zealot.co.jp>
2021-11-01 16:57:21 +0900

    add rollup

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -8,14 +8,25 @@ export default {
         '@snowpack/plugin-svelte',
         '@snowpack/plugin-dotenv',
         '@snowpack/plugin-typescript',
+        [
+            'snowpack-plugin-rollup-bundle',
+            {
+                emitHtmlFiles: true,
+                preserveSourceFiles: true,
+                entrypoints: 'build/dist/index.js',
+            }
+        ]
     ],
     routes: [
         /* Enable an SPA Fallback in development: */
         // {"match": "routes", "src": ".*", "dest": "/index.html"},
     ],
     optimize: {
-        /* Example: Bundle your final build: */
-        // "bundle": true,
+        // esbuild is still young and not yet production-ready.
+        // https://www.snowpack.dev/guides/optimize-and-bundle
+        // bundle: true,
+        // minify: true,
+        // target: 'es2020',
     },
     packageOptions: {
         /* ... */
